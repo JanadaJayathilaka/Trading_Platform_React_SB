866f12ebb5f755f6494c1a87ccd5b48fc05ecf1c
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-05 18:40:32 +0530

    create wallet

---
+++ trading/src/main/java/com/example/trading/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.example.trading.domain;
+
+public enum OrderStatus {
+    PENDING,FILLED,CANCELLED,PARTIALLY_FILLED,ERROR,SUCCESS
+}
---
+++ trading/src/main/java/com/example/trading/domain/OrderType.java
@@ -0,0 +1,6 @@
+package com.example.trading.domain;
+
+public enum OrderType {
+    BUY,
+    SELL
+}
