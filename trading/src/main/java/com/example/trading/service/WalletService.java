560bcd26ab3edde88a58bd4d07a48630bb34584e
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-05 12:34:56 +0530

    wallet service interface methods initialized

--- trading/src/main/java/com/example/trading/service/WalletService.java
+++ trading/src/main/java/com/example/trading/service/WalletService.java
@@ -1,4 +1,18 @@
 package com.example.trading.service;
 
+import com.example.trading.model.Order;
+import com.example.trading.model.User;
+import com.example.trading.model.Wallet;
+
 public interface WalletService {
+    Wallet getUserWallet(User user);
+
+    Wallet addBalance(Wallet wallet,Long money);
+
+    Wallet findWalletById(Long id);
+
+    Wallet walletToWalletTransfer(User sender, Wallet receiverWallet,Long amount);
+
+    Wallet payOrderPayment(Order order, User user);
+
 }
