551872ecfcc77d24904e270207fb0f6f9190ee2a
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-05 10:01:44 +0530

    Create methods for coin service

---
+++ trading/src/main/java/com/example/trading/service/CoinService.java
@@ -0,0 +1,22 @@
+package com.example.trading.service;
+
+import com.example.trading.model.Coin;
+
+import java.util.List;
+
+public interface CoinService {
+
+    List<Coin> getCoinList(int page);
+
+    String getMarketChart(String coinId, int days);
+
+    String getCoinDetails(String coinId);
+
+    Coin findById(String coinId);
+
+    String searchCoin(String keyword);
+
+    String getTop50CoinsByMarketCapRank();
+
+    String getTradingCoins();
+}
