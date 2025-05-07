0cef0ec62dc9c6e61afc32569a7e937095c9b548
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-07 18:59:46 +0530

    create methods for AssetServiceImpl.java

--- trading/src/main/java/com/example/trading/service/AssetService.java
+++ trading/src/main/java/com/example/trading/service/AssetService.java
@@ -9,13 +9,13 @@ import java.util.List;
 public interface AssetService {
     Asset createAsset(User user, Coin coin, double quantity);
 
-    Asset getAssetById(Long assetId);
+    Asset getAssetById(Long assetId) throws Exception;
 
     Asset getAssetByUserId(Long userId, Long assetId);
 
     List<Asset> getUsersAssets(Long userId);
 
-    Asset updateAsset(Long assetId,double quantity);
+    Asset updateAsset(Long assetId,double quantity) throws Exception;
 
     Asset findAssetByUserIdAndCoinId(Long userId,String coinId);
 
