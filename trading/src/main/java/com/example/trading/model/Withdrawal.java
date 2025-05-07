7abc83f5c95abc3943bcd806cfcbb6fdec2eca39
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-07 20:38:39 +0530

    finish withdrawa model

---
+++ trading/src/main/java/com/example/trading/model/Withdrawal.java
@@ -0,0 +1,66 @@
+package com.example.trading.model;
+
+
+import com.example.trading.domain.WithdrawalStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Withdrawal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private WithdrawalStatus status;
+
+    private Long amount;
+
+    @ManyToOne
+    private User user;
+
+    private LocalDateTime date = LocalDateTime.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public WithdrawalStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(WithdrawalStatus status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+}
+
