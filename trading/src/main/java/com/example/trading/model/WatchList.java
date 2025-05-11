81b0cd76ef23a34f32ff64b61c98dc8415c18299
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-11 13:17:05 +0530

    create watch list model

---
+++ trading/src/main/java/com/example/trading/model/WatchList.java
@@ -0,0 +1,27 @@
+package com.example.trading.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+public class WatchList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+    @ManyToMany
+    private List<Coin> coins =  new ArrayList<>();
+
+
+
+
+}
