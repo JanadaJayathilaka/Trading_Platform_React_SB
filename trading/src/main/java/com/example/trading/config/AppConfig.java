5bfbf2d53d3eb8bb9ceb17915082ad8b4746ca5f
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-05-21 15:43:18 +0530

    update CORS configuration to allow requests from localhost:5174

--- trading/src/main/java/com/example/trading/config/AppConfig.java
+++ trading/src/main/java/com/example/trading/config/AppConfig.java
@@ -37,7 +37,7 @@ public class AppConfig {
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration cfg = new CorsConfiguration();
                 cfg.setAllowedOrigins(
-                        Arrays.asList( "http://localhost:5173", "http://localhost:8080")
+                        Arrays.asList( "http://localhost:5173", "http://localhost:5174")
                 );
                 cfg.setAllowedMethods(Collections.singletonList("*"));
                 cfg.setAllowCredentials(true);
